87906a3e24ce9d4a1e7dd4b94f9a77dd7de348de
arturo <ahpareja@gmail.com>
2022-06-27 17:51:50 -0500

    Add home page animations and styling

---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+import './index.scss';
+import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>Hi, <br/> I'm
+        <img src={LogoTitle} alt="developer"/>
+        rturo
+        <br/>
+        Full-Stack Web Developer
+        </h1>
+        <h2>Reac-Redux / Ruby on rails Developer</h2>
+        <Link to="/contact" className="flat-button">CONTACT ME</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
