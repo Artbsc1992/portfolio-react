a9a2d8b3016fc67e776a9d0cf240367f64c27acf
arturo <ahpareja@gmail.com>
2022-06-28 11:27:13 -0500

    cube change icons

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,14 @@ import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faHtml5, faCss3, faReact, faGitAlt, faJsSquare } from '@fortawesome/free-brands-svg-icons';
+import { 
+faGithub, 
+faHtml5, 
+faCss3, 
+faReact, 
+faGitAlt, 
+faJsSquare 
+} from '@fortawesome/free-brands-svg-icons';
 import Loader from 'react-loaders';
 
 const About = () => {
@@ -45,22 +52,22 @@ const About = () => {
         <div className='stage-cube-cont'>
           <div className='cubespinner'>
             <div className='face1'>
-              <FontAwesomeIcon icon={faReact} color='#5ed4f4'/>
+              <FontAwesomeIcon icon={faGithub} color='#fff'/>
             </div>
             <div className='face2'>
               <FontAwesomeIcon icon={faHtml5} color='#f06529' />
             </div>
             <div className='face3'>
-              <FontAwesomeIcon icon={faCss3} color='#28a4d9' />
-            </div>
-            <div className='face4'>
               <FontAwesomeIcon icon={faJsSquare} color='#efd81d' />
             </div>
+            <div className='face4'>
+              <FontAwesomeIcon icon={faGitAlt} color='#ec4d28' />
+            </div>
             <div className='face5'>
-              <FontAwesomeIcon icon={faGithub} color='#fff' />
+              <FontAwesomeIcon icon={faCss3} color='#28a4d9' />
             </div>
             <div className='face6'>
-              <FontAwesomeIcon icon={faGitAlt} color='#ec4d28' />
+              <FontAwesomeIcon icon={faReact} color='#5ed4f4' />
             </div>
           </div>
         </div>
