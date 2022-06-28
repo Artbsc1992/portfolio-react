1cc2468eff5b04aa88c81ef4947758dd2fda3e04
arturo <ahpareja@gmail.com>
2022-06-27 19:12:03 -0500

    add letter animations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,37 @@
 import './index.scss';
+import { useEffect, useState } from 'react';
 import LogoTitle from '../../assets/images/logo-s.png';
 import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+  const [ letterClass, setLetterClass ] = useState('text-animate')
+  const nameArray = ['r', 't', 'u', 'r', 'o'];
+  const jobArray = ['F', 'u', 'l', 'l', '-', 'S', 't', 'a', 'c', 'k', ' ', 'W', 'e', 'b', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
+
+  useEffect (() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4500)
+  }, []);
+
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br/> I'm
-        <img src={LogoTitle} alt="developer"/>
-        rturo
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span> 
         <br/>
-        Full-Stack Web Developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span> 
+        <img src={LogoTitle} alt="developer"/>
+        <AnimatedLetters letterClass={letterClass} 
+        strArray={nameArray}
+        idx={15} />
+        <br/>
+        <AnimatedLetters letterClass={letterClass} 
+        strArray={jobArray}
+        idx={22} />
         </h1>
         <h2>Reac-Redux / Ruby on rails Developer</h2>
         <Link to="/contact" className="flat-button">CONTACT ME</Link>
